f309f8da6f68463d45e094e49f4eb68ebb613c05
Sikander Chowhan <sikanderchowhan@gmail.com>
2017-08-16 23:30:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.size
+    item = yield collection[i]
+    new_collection.push(item)
+    i = i + 1
+  end
+  new_collection
+end
